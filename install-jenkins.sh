d330d47e8c444eeea350a8e9eeb24f730b03ef80
sai <saiteja121020@gmail.com>
2026-07-22 16:30:07 -0400

    Ticket Number: Made Necessary changes in Jenkins File

--- install-jenkins.sh
+++ install-jenkins.sh
@@ -3,27 +3,33 @@
 set -e
 
 echo "Updating packages..."
-
 sudo apt update -y
 
 echo "Installing Java..."
+sudo apt install -y openjdk-21-jdk curl gnupg
 
-sudo apt install openjdk-21-jdk -y
+echo "Removing old Jenkins repository (if any)..."
+sudo rm -f /etc/apt/sources.list.d/jenkins.list
+sudo rm -f /usr/share/keyrings/jenkins-keyring.asc
+sudo rm -f /usr/share/keyrings/jenkins-keyring.gpg
 
-echo "Adding Jenkins Repository..."
+echo "Adding Jenkins GPG key..."
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key \
+| gpg --dearmor \
+| sudo tee /usr/share/keyrings/jenkins-keyring.gpg > /dev/null
 
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo "Adding Jenkins repository..."
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.gpg] https://pkg.jenkins.io/debian-stable binary/" \
+| sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
 
-echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
-
+echo "Updating package list..."
 sudo apt update
 
 echo "Installing Jenkins..."
+sudo apt install -y jenkins
 
-sudo apt install jenkins -y
-
+echo "Starting Jenkins..."
 sudo systemctl enable jenkins
-
 sudo systemctl start jenkins
 
 echo "Jenkins Installed Successfully!"
